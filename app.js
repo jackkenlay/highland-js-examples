446a5db29cf647880796ebc8570a2cc3fe4d6a9f
Jack Kenlay <jack.kenlay@reachplc.com>
2020-05-06 15:44:56 +0100

    Example

--- app.js
+++ app.js
@@ -1,35 +1,51 @@
-const _ = require('highland');
+const H = require('highland');
 
 
-//Create Array
-let test = [];
-for(let i = 0; i < 100; i++){
-    test.push(i);
-}
+//Create Array (numbers
+// let test = [];
+// for(let i = 0; i < 100; i++){
+//     test.push(i);
+// }
 
 //Stream it with rate of 1, 1000
-var nums = _(test).map(function (x) {
-    return Number(x);
-})
-.ratelimit(1, 1000)
+// var nums = H(test).map(function (x) {
+//     return Number(x);
+// })
 
-//Highland functions, add in anything you want:
+let test = [{name:'jack', age:6},{name:'dave', age:9}, {name:'tom', age:12}]
 
+console.log('input to stream', test)
 
+H(test)
+.ratelimit(1, 1000) //passes objects down the streams 1 per second
+
+//Highland functions, add in anything you want below:
+
+.tap(item => console.log('current item Line 22:',item))
 // .batchWithTimeOrCount(4000, 2)
-.doto ( items => console.log ( `First Doto: ${items}` ) )
+// .doto ( items => console.log ( `First Doto: ${items}` ) )
 // .filter(function (x) {
 //     return x % 2 === 0;
 // })
-.doto ( items => console.log ( `Second Doto: ${items}` ) )
+.flatMap(item =>{
+    //flat maps must return a stream
+    item.hobby = 'coding'
+    return H([item])
+})
+.map(item => {
+    item.age = 9;
+    return item;
+})
+// .tap(item => console.log('current item line 33:',item))
+// .doto ( items => console.log ( `Second Doto: ${items}` ) )
 // .consume(function (err, x, push, next) {
 //     console.log('consume');
 // });
+.collect()
+.toCallback((err, result)=>{
+    console.log('end of stream', result)
+})
 
-console.log('After stream:')
-nums.each(function (n) { 
-    console.log(n); 
-});
 
 
 //this is used to keep Node active and stop it from exiting
